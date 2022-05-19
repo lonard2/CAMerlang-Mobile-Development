e0570e4a488633ce838f6e8ccfe9230b98e5e3e9
Lonard Steven <lonard.steven@binus.ac.id>
2022-05-19 11:14:25 +0700

    Fixing XML issues on rounded box shape

--- app/src/main/java/com/lonard/camerlangproject/db/consultation/ConsultationDao.kt
+++ app/src/main/java/com/lonard/camerlangproject/db/consultation/ConsultationDao.kt
@@ -1,4 +1,5 @@
 package com.lonard.camerlangproject.db.consultation
 
 class ConsultationDao {
+
 }
--- app/src/main/java/com/lonard/camerlangproject/ui/ConsultationActivity.kt
+++ app/src/main/java/com/lonard/camerlangproject/ui/ConsultationActivity.kt
@@ -2,6 +2,7 @@ package com.lonard.camerlangproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lonard.camerlangproject.R
 
 class ConsultationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
