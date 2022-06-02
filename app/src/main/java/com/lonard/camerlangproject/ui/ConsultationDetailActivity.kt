bd8fe27c3f0a5c9e58c7f4501e5257e2b9a0d51b
Lonard Steven <lonard.steven@binus.ac.id>
2022-06-02 17:05:41 +0700

    Building consultation detail activity with data (from its view binding)

--- app/src/main/java/com/lonard/camerlangproject/ui/ConsultationDetailActivity.kt
+++ app/src/main/java/com/lonard/camerlangproject/ui/ConsultationDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.lonard.camerlangproject.R
 import com.lonard.camerlangproject.databinding.ActivityConsultationDetailBinding
 import com.lonard.camerlangproject.databinding.ActivityCreditsBinding
+import com.squareup.picasso.Picasso
 
 class ConsultationDetailActivity : AppCompatActivity() {
     private lateinit var bind: ActivityConsultationDetailBinding
@@ -13,5 +14,31 @@ class ConsultationDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityConsultationDetailBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
+        bind.apply {
+            backBtn.setOnClickListener {
+                finish()
+            }
+        }
+
+        bind.apply {
+            Picasso.get().load(analyzedImg).into(consultationTakenImage)
+
+            consultationIdDetail.text = analyzedConsultationId
+            consultationDatetimeDetail.text = analyzedConsultationDateTime
+            consultationOutcomeDetail.text = analyzedConsultationOutcome
+
+            firstProblemShow.text = retrievedTfResultFirstProblem
+            secondProblemShow.text = retrievedTfResultSecondProblem
+            thirdProblemShow.text = retrievedTfResultThirdProblem
+
+            firstProblemPercentageTxt.text = retrievedTfResultFirstProblemPercentage
+            secondProblemPercentageTxt.text = retrievedTfResultSecondProblemPercentage
+            thirdProblemPercentageTxt.text = retrievedTfResultThirdProblemPercentage
+        }
+    }
+
+    companion object {
+        const val EXTRA_CONSULTATION_DATA = "captured_information"
     }
 }
