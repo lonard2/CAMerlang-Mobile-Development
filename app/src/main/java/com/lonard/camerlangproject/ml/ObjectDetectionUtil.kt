daaa8b8afd119e3b4ae208d7e7f483e2078243ef
arya-fh-n <aryafikry86@gmail.com>
2022-06-05 16:26:19 +0700

    Add more objectdetection code and implement boundingbox draw on image

--- app/src/main/java/com/lonard/camerlangproject/ml/ObjectDetectionUtil.kt
+++ app/src/main/java/com/lonard/camerlangproject/ml/ObjectDetectionUtil.kt
@@ -1,6 +1,7 @@
 package com.lonard.camerlangproject.ml
 
 import android.content.Context
+import android.graphics.*
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -9,8 +10,9 @@ import org.tensorflow.lite.task.vision.detector.ObjectDetector
 class ObjectDetectionUtil {
 
     private lateinit var context: Context
+    lateinit var resultBitmap: Bitmap
 
-    private fun detectionProcess() {
+    private fun objectDetection(bitmap: Bitmap) {
         val retrievedTakenImage = TensorImage.fromBitmap(bitmap)
 
         val objectDetectionOptions = ObjectDetector.ObjectDetectorOptions.builder()
@@ -25,6 +27,54 @@ class ObjectDetectionUtil {
         )
 
         val detectionResults = detector.detect(retrievedTakenImage)
+
+        val displayResults = detectionResults.map { detection ->
+            val category = detection.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            DetectionResult(detection.boundingBox, text)
+        }
+
+        val imgWithResult = drawDetectionResult(bitmap, displayResults)
+        resultBitmap = imgWithResult
+    }
+
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = 32F
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
     }
 
 //    private fun detectionProcess() {
