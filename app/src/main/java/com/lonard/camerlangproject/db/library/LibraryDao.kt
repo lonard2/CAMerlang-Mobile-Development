18c47afa8e8d8481e043381adadddd81717d0c51
arya-fh-n <aryafikry86@gmail.com>
2022-06-08 16:17:26 +0700

    Add Dao annotation (forgot to add the annotation before)

--- app/src/main/java/com/lonard/camerlangproject/db/library/LibraryDao.kt
+++ app/src/main/java/com/lonard/camerlangproject/db/library/LibraryDao.kt
@@ -1,10 +1,12 @@
 package com.lonard.camerlangproject.db.library
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface LibraryDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
