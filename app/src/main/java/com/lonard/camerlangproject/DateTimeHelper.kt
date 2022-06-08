dcc6319333033adfc4289938d0d640d43d7b7116
Lonard Steven <lonard.steven@binus.ac.id>
2022-06-08 16:31:10 +0700

    Adding date-time helper for modifying API-sourced date-time data

---
+++ app/src/main/java/com/lonard/camerlangproject/DateTimeHelper.kt
@@ -0,0 +1,12 @@
+package com.lonard.camerlangproject
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun String.formatDateTime(): String {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
+    val date = dateFormat.parse(this) as Date
+
+    return DateFormat.getDateInstance(DateFormat.FULL).format(date) + " " + DateFormat.getTimeInstance(DateFormat.FULL).format(date)
+}
